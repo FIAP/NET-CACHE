8c54c9e56a2abfef7d20a773891e7549540d0d1d
Mestre do Rafa <tadriano.net@gmail.com>
2024-08-29 18:46:03 -0300

    init

---
+++ dotnet-cache/Service/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using dotnetCache.Models;
+
+namespace dotnetCache.Service;
+
+public interface IProductService
+{
+    IEnumerable<Product> GetProducts();
+    Product GetProductById(int id);
+
+    void CreateProduct(Product product);
+}
